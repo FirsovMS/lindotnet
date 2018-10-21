a0619740df7bc78acd59f2df0f9ca5ed5c406754
FirsovMS <firsovms1993@gmail.com>
2018-10-21 12:31:57 +0400

    upd

---
+++ TestProject/TestCreateSoftphoneInstance.cs
@@ -0,0 +1,24 @@
+﻿using lindotnet;
+using lindotnet.Classes.Component.Implementation;
+using lindotnet.Classes.Component.Interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProject
+{
+    [TestClass]
+    public class TestCreateSoftphoneInstance
+    {
+        [TestMethod]
+        public void SoftphoneInstanceCreate()
+        {
+            // using office sip for testing
+            var testAccount = new Account(
+                login: "test",
+                password: "testpass",
+                host: "officesip.local",
+                accountName: "TestUser");
+
+            ISoftphone softphoneInstance = Container.CreateNewSoftphone(testAccount);
+        }
+    }
+}
---
+++ TestProject/TestSoftphoneConnect.cs
@@ -0,0 +1,40 @@
+﻿using lindotnet;
+using lindotnet.Classes.Component.Implementation;
+using lindotnet.Classes.Component.Interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+
+namespace TestProject
+{
+    [TestClass]
+    public class TestSoftphoneConnect
+    {
+        public Account testAccount { get; private set; }
+        public ISoftphone softphoneInstance { get; private set; }
+
+        [SetUp]
+        public void CreateInstances()
+        {
+            // using office sip for testing
+            testAccount = new Account(
+                login: "test",
+                password: "testpass",
+                host: "officesip.local",
+                accountName: "TestUser");
+
+            softphoneInstance = Container.CreateNewSoftphone(testAccount);
+        }
+
+        [TestMethod]
+        public void TestConnect()
+        {
+            softphoneInstance.
+        }
+
+        [TearDown]
+        public void DestroyInstances()
+        {
+            
+        }
+    }
+}
