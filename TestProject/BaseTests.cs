e16968941af4bceb51e0f758eb8bd6f11b43a4b9
FirsovMS <firsovms1993@gmail.com>
2018-10-23 04:45:51 +0400

    updated NLog.config

---
+++ TestProject/BaseTests.cs
@@ -0,0 +1,90 @@
+﻿using lindotnet.Classes.Component.Implementation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+	[TestClass]
+	public class BaseTests
+	{
+		private static readonly TimeSpan ConnectionDelay = TimeSpan.FromSeconds(2);
+
+		[TestMethod]
+		public void CreateInstances()
+		{
+			// using office sip for testing
+			var testAccount = new Account(
+				login: "test",
+				password: "testpass",
+				host: "officesip.local",
+				accountName: "TestUser");
+
+			var softphoneInstance = new Softphone(testAccount);
+		}
+
+		[TestMethod]
+		public void TestPhoneConnectionOnServer()
+		{
+			// using local sip server for testing
+			var testAccount = new Account(
+				login: "test",
+				password: "testpass",
+				host: "192.168.156.2",
+				accountName: "TestUser");
+
+			var softphoneInstance = new Softphone(testAccount);
+
+			try
+			{
+				softphoneInstance.Connect();
+
+				var delayTask = Task.Delay(ConnectionDelay);
+				delayTask.Wait();
+
+				Assert.AreEqual(lindotnet.Classes.ConnectState.Connected, softphoneInstance.ConnectState);
+			}
+			catch (Exception e)
+			{
+				Assert.Fail($"TestPhoneConnectionOnServer Failed: {e.Message}");
+			}
+			finally
+			{
+				softphoneInstance.Disconnect();
+			}
+		}
+
+		[TestMethod]
+		public void TestCallToAnotherSoftphone()
+		{
+			// using local sip server for testing
+			var testAccount = new Account(
+				login: "test",
+				password: "testpass",
+				host: "192.168.156.2",
+				accountName: "TestUser");
+
+			var softphoneInstance = new Softphone(testAccount);
+
+			try
+			{
+				softphoneInstance.Connect();
+
+				var delayTask = Task.Delay(ConnectionDelay);
+				delayTask.Wait();
+
+				softphoneInstance.MakeCall("100");
+
+				Assert.AreEqual(lindotnet.Classes.ConnectState.Connected, softphoneInstance.ConnectState);
+			}
+			catch (Exception e)
+			{
+				Assert.Fail($"TestPhoneConnectionOnServer Failed: {e.Message}");
+			}
+			finally
+			{
+				softphoneInstance.Disconnect();
+			}
+		}
+	}
+}
