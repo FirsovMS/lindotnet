37d19354418ba4b0b72e5b160ddbcfe574b2b878
FirsovMS <firsovms1993@gmail.com>
2018-10-23 05:52:51 +0400

    Logger Succesfuly tested, edit commit;
    TestCallToAnotherSoftphone fails, because ActualCall variable not assigned

--- TestProject/TestLogger.cs
+++ TestProject/TestLogger.cs
@@ -1,19 +1,67 @@
-﻿using LoggingAPI;
+﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LoggingAPI;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace TestProject
 {
 	[TestClass]
 	public class TestLogger
 	{
-		[TestMethod]
-		public void TestLoggerInfo()
+		private static readonly TimeSpan WaitTime = TimeSpan.FromSeconds(5);
+
+		private static readonly string workingDir = Environment.CurrentDirectory + @"\logs\";
+
+		private static string LogFileName(DateTime dateTime)
 		{
-			string mock = "dummy: testing logger info";
+			return dateTime.ToString("yyyy-MM-dd");
+		}
+
+		[TestMethod]
+		public void TestLogInfo()
+		{
+			string mock = "info: mock example bla bla bla";
+
+			string fileName = LogFileName(DateTime.Now) + ".log";
 
 			Logger.Info(mock);
 
+			string path = workingDir + fileName;
 
+			Task.Delay(WaitTime).Wait();
+
+			if (File.Exists(path))
+			{
+				Assert.IsTrue(File.ReadAllText(path).Contains(mock));
+			}
+			else
+			{
+				throw new FileNotFoundException("Log file not created!");
+			}
+		}
+
+		[TestMethod]
+		public void TestLogError()
+		{
+			string mock = "error: mock example bla bla bla";
+
+			string fileName = LogFileName(DateTime.Now) + ".log";
+
+			Logger.Error(mock, new Exception(mock), Level.Fatal);
+
+			string path = workingDir + fileName;
+
+			Task.Delay(WaitTime).Wait();
+
+			if (File.Exists(path))
+			{
+				Assert.IsTrue(File.ReadAllText(path).Contains(mock));
+			}
+			else
+			{
+				throw new FileNotFoundException("Log file not created!");
+			}
 		}
 	}
 }
---
+++ TestProject/TestSoftphone.cs
@@ -0,0 +1,96 @@
+﻿using lindotnet.Classes.Component.Implementation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+	[TestClass]
+	public class TestSoftphone
+	{
+		private static readonly TimeSpan ConnectionDelay = TimeSpan.FromSeconds(2);
+		private static readonly string ExampleURI = "music@iptel.org";
+
+		[TestMethod]
+		public void CreateSoftphoneInstance()
+		{
+			// using office sip for testing
+			var testAccount = new Account(
+				login: "test",
+				password: "testpass",
+				host: "192.168.156.2",
+				accountName: "TestUser");
+
+			var softphoneInstance = new Softphone(testAccount);
+		}
+
+		[TestMethod]
+		public void TestSoftphoneServerConnection()
+		{
+			Softphone softphoneInstance = null;
+			try
+			{
+				// using office sip for testing
+				var testAccount = new Account(
+					login: "test",
+					password: "testpass",
+					host: "192.168.156.2",
+					accountName: "TestUser");
+
+				softphoneInstance = new Softphone(testAccount);
+
+				softphoneInstance.Connect();
+
+				Task.Delay(ConnectionDelay).Wait();
+
+				Assert.AreEqual(lindotnet.Classes.ConnectState.Connected, softphoneInstance.ConnectState);
+
+			}
+			catch (Exception e)
+			{
+				Assert.Fail(e.Message);
+			}
+			finally
+			{
+				softphoneInstance?.Disconnect();
+			}
+		}
+
+		[TestMethod]
+		public void TestCallToAnotherSoftphone()
+		{
+			Softphone softphoneInstance = null;
+			try
+			{
+				// using office sip for testing
+				var testAccount = new Account(
+					login: "test",
+					password: "testpass",
+					host: "192.168.156.2",
+					accountName: "TestUser");
+
+				softphoneInstance = new Softphone(testAccount);
+
+				softphoneInstance.Connect();
+
+				Task.Delay(ConnectionDelay).Wait();
+
+				softphoneInstance.MakeCall(ExampleURI);
+
+				Assert.AreEqual(lindotnet.Classes.LineState.Busy, softphoneInstance.LineState);
+				if(softphoneInstance.ActiveCall != null)
+				{
+
+				}
+			}
+			catch (Exception e)
+			{
+				Assert.Fail(e.Message);
+			}
+			finally
+			{
+				softphoneInstance?.Disconnect();
+			}
+		}
+	}
+}
